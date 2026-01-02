e8ad2e01f9bf80e1e65abb4753997c81228cfdbe
Tisha Carpenter <tisha.carpenter.417@gmail.com>
2026-01-02 19:34:29 +0530

    Delete division.py

--- division.py
+++
@@ -1,9 +0,0 @@
-a= float(input("type num1:"))
-b= float(input("type num2:"))
-
-try:
-    result= a/b
-except ZeroDivisionError:
-     print("Error! \nCannot divide by zero") 
-else:
-     print(result)
